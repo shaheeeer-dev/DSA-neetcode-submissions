deb3139aed7a48de2f4fa340d9a30bcbb2d4d336
Muhammad Shaheer <132088340+shaheeeer-dev@users.noreply.github.com>
2026-05-21 13:07:56 +0500

    Add: evaluate-reverse-polish-notation - submission-0

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i<tokens.length; i++){
+            if(tokens[i].equals("+") || tokens[i].equals("-") || tokens[i].equals("*") || tokens[i].equals("/")){
+                int b = st.pop();
+                int a = st.pop();
+                if (tokens[i].equals("+")) {
+                    st.push(a + b);
+                }
+                else if (tokens[i].equals("-")) {
+                    st.push(a - b);
+                }
+                else if (tokens[i].equals("*")) {
+                    st.push(a * b);
+                }
+                else {
+                    st.push(a / b);
+                }
+            }
+            else {
+                st.push(Integer.parseInt(tokens[i]));
+            }
+        }
+        return st.peek();
+    }
+}
